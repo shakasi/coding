163ed649421b39c9167b0d4ca78321365377b714
shakasi <shakasi@163.com>
2021-01-19 15:07:31 +0800

    .

---
+++ CoreTest/CoreTest/EF/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.EF
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<User> Users { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          => optionsBuilder
+              .UseSqlite(@"data source=Sqlites\\docare.db;");
+    }
+}
