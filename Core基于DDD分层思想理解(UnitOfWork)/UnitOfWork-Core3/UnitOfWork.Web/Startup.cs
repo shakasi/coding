5470af747dff7ef25bf3f261b2b07362885a7e3b
shakasi <shakasi@ 163.com>
2021-02-03 15:39:53 +0800

    .Net Core和EF Core基于DDD分层思想实现的 UnitOfWork

--- Core基于DDD分层思想理解(UnitOfWork)/UnitOfWork-Core3/UnitOfWork.Web/Startup.cs
+++ Core基于DDD分层思想理解(UnitOfWork)/UnitOfWork-Core3/UnitOfWork.Web/Startup.cs
@@ -5,11 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UnitOfWork.Customer;
+using UnitOfWork.Repositories;
 
 namespace UnitOfWork.Web
 {
@@ -26,6 +24,29 @@ namespace UnitOfWork.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //This is the only service available at ConfigureServices
+
+            //使用内存数据库
+            //var connection = new SqliteConnection(Configuration.GetConnectionString("InMemoryConnection"));
+            //connection.Open();
+            //services.AddDbContext<UnitOfWorkDbContext>(options =>
+            //    options.UseSqlite(connection));
+
+            services.AddDbContext<UnitOfWorkDbContext>(options =>
+                //options.UseSqlServer(Configuration.GetConnectionString("SqliteConnection")));
+                options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
+
+            //使用扩展方法注入Uow依赖
+            services.AddUnitOfWork<UnitOfWorkDbContext>();
+            //使用默认方法注入Uow依赖
+            //services.AddScoped<IUnitOfWork, UnitOfWork<UnitOfWorkDbContext>>();
+
+            //注入泛型仓储
+            services.AddTransient(typeof(IRepository<>), typeof(EfCoreRepository<>));
+            services.AddTransient(typeof(IRepository<,>), typeof(EfCoreRepository<,>));
+
+            services.AddTransient<ICustomerAppService, CustomerAppService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
